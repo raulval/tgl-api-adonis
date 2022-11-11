078a437a7c845bd6497702c039a4a5fe89a65dbc
Raul Val <raulval@outlook.com>
2022-11-11 10:42:38 -0300

    ✨ create seeders for Cart, Game and User

---
+++ database/seeders/User.ts
@@ -0,0 +1,16 @@
+import BaseSeeder from "@ioc:Adonis/Lucid/Seeder";
+import User from "App/Models/User";
+
+export default class extends BaseSeeder {
+  public async run() {
+    // Write your database queries inside the run method
+
+    await User.firstOrCreate({
+      id: 1,
+      email: "admin@luby.com",
+      password: "secret",
+      is_admin: true,
+      name: "LubyTGL",
+    });
+  }
+}
