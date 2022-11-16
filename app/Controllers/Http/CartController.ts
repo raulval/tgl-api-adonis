815b4d5155e36bee0f9ed25568767feee6435b32
Raul Val <raulval@outlook.com>
2022-11-16 14:54:50 -0300

    ✨ create cart controller

---
+++ app/Controllers/Http/CartController.ts
@@ -0,0 +1,25 @@
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import Cart from "App/Models/Cart";
+import Game from "App/Models/Game";
+
+export default class CartController {
+  public async index({}: HttpContextContract) {
+    let cart = await Cart.query().select("min_cart_value").first();
+
+    const cartJSON = cart ? cart.serialize() : {};
+
+    const games = await Game.query().select(
+      "id",
+      "type",
+      "description",
+      "range",
+      "price",
+      "max_number",
+      "color"
+    );
+
+    cartJSON.types = games;
+
+    return cartJSON;
+  }
+}
