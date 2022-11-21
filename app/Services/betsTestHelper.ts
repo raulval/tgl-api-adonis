7f949b6e0a889158bafc440c70d080d459fb74e9
Raul Val <raulval@outlook.com>
2022-11-21 13:57:18 -0300

    ✅ create test for bets

---
+++ app/Services/betsTestHelper.ts
@@ -0,0 +1,44 @@
+export const bets = {
+  games: [
+    {
+      game_id: 1,
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+    },
+    {
+      game_id: 1,
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+    },
+    {
+      game_id: 2,
+      numbers: [1, 2, 3, 4, 5, 6],
+    },
+    {
+      game_id: 2,
+      numbers: [1, 2, 3, 4, 5, 6],
+    },
+    {
+      game_id: 2,
+      numbers: [1, 2, 3, 4, 5, 6],
+    },
+    {
+      game_id: 2,
+      numbers: [1, 2, 3, 4, 5, 6],
+    },
+    {
+      game_id: 2,
+      numbers: [1, 2, 3, 4, 5, 6],
+    },
+    {
+      game_id: 3,
+      numbers: [1, 2, 3, 4, 5],
+    },
+    {
+      game_id: 3,
+      numbers: [1, 2, 3, 4, 5],
+    },
+    {
+      game_id: 3,
+      numbers: [1, 2, 3, 4, 5],
+    },
+  ],
+};
---
+++ tests/functional/bet/create.spec.ts
@@ -0,0 +1,61 @@
+import Database from "@ioc:Adonis/Lucid/Database";
+import { test } from "@japa/runner";
+import User from "App/Models/User";
+import { bets } from "App/Services/betsTestHelper";
+
+test.group("Create bet", (group) => {
+  group.tap((test) => test.tags(["@bet_create"]));
+
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction();
+    return () => Database.rollbackGlobalTransaction();
+  });
+
+  const resource = "/bet/new-bet";
+
+  test("should create bet with valid data and min cart value", async ({
+    client,
+  }) => {
+    const admin = await User.findOrFail(1);
+
+    const response = await client.post(resource).json(bets).loginAs(admin);
+
+    response.assertStatus(200);
+  });
+
+  test("should not create bet with user not authorized", async ({ client }) => {
+    const response = await client.post(resource).json(bets);
+
+    response.assertStatus(401);
+    response.assertBodyContains({
+      errors: [
+        {
+          message: "E_UNAUTHORIZED_ACCESS: Unauthorized access",
+        },
+      ],
+    });
+  });
+
+  test("should not create bet with value below min cart value", async ({
+    client,
+  }) => {
+    const admin = await User.findOrFail(1);
+
+    const response = await client
+      .post(resource)
+      .json({
+        games: [
+          {
+            game_id: 1,
+            numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15],
+          },
+        ],
+      })
+      .loginAs(admin);
+
+    response.assertStatus(400);
+    response.assertBodyContains({
+      message: "The value min authorized is R$ 30,00",
+    });
+  });
+});
